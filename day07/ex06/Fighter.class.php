e462f061c92b55117791391388bd92dd5dc47442
JonathanLimbada <50628608+JonathanLimbada@users.noreply.github.com>
2019-10-22 08:00:25 +0200

    day05 to day09

---
+++ day07/ex06/Fighter.class.php
@@ -0,0 +1,16 @@
+  
+<?php
+abstract class Fighter
+{
+	private $div;
+	public function __construct($type)
+	{
+		$this->div = $type;
+	}
+	abstract function fight($target);
+	public function getDiv()
+	{
+		return($this->div);
+	}
+}
+?>
